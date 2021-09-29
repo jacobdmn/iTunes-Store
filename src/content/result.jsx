3ed276e29b4c2de1f0d6b1ab73e943b846795d9c
Jacob GI <jacob.demmane@gmail.com>
2021-09-29 23:01:54 +0300

    svgs set

--- src/content/result.jsx
+++ src/content/result.jsx
@@ -18,7 +18,19 @@ const Result = ({ song: { title, artist, album, image } }) => {
         </div>
       </div>
       <button className='love'>
-        <img src='./imgs/love_icon.svg' alt='' />
+        <svg
+          // style={{enableBackground:new 0 0 512 512;}}
+          version='1.1'
+          viewBox='0 0 512 512'
+          width='512px'
+          xmlns='http://www.w3.org/2000/svg'>
+          <g id='_x31_66_x2C__Heart_x2C__Love_x2C__Like_x2C__Twitter'>
+            <g>
+              <path d='M365.4,59.628c60.56,0,109.6,49.03,109.6,109.47c0,109.47-109.6,171.8-219.06,281.271    C146.47,340.898,37,278.568,37,169.099c0-60.44,49.04-109.47,109.47-109.47c54.73,0,82.1,27.37,109.47,82.1    C283.3,86.999,310.67,59.628,365.4,59.628z' />
+            </g>
+          </g>
+          <g id='Layer_1' />
+        </svg>
       </button>
     </div>
   );
--- src/header/header.jsx
+++ src/header/header.jsx
@@ -10,7 +10,22 @@ const MyHeader = () => {
       </div>
       <div className='menu'>
         <div className='search-bar active'>
-          <a href=''>Search a song</a>
+          <a href=''>
+            <svg
+              fill='none'
+              height='24'
+              stroke='#000'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='2'
+              viewBox='0 0 24 24'
+              width='24'
+              xmlns='http://www.w3.org/2000/svg'>
+              <circle cx='10.5' cy='10.5' r='7.5' />
+              <line x1='21' x2='15.8' y1='21' y2='15.8' />
+            </svg>
+            Search a song
+          </a>
         </div>
         <div className='history'>
           <a href=''>History</a>
