c6483f5847c64d75c5ed9b2ff04a49c67ec33e63
Jacob GI <jacob.demmane@gmail.com>
2021-10-03 21:12:12 +0300

    5 most favorite artists DONE

--- src/content/statistics.jsx
+++ src/content/statistics.jsx
@@ -15,65 +15,96 @@ const Statistics = () => {
     let edgesValueWithoutLabel = [];
 
     /// get needed data and set it into arrays
-    favoriteList
-      .slice(0, 6)
-      .forEach(
-        (
+    favoriteList.forEach(
+      (
+        {
+          trackId,
+          trackName,
+          trackImage,
+          trackImageIcon,
+          albumId,
+          albumName,
+          artistId,
+          artistName,
+        },
+        index
+      ) => {
+        artists = [
+          ...artists,
           {
-            trackId,
-            trackName,
-            trackImage,
-            albumId,
-            albumName,
-            artistId,
-            artistName,
+            id: artistId,
+            label: `Artist: ${artistName.slice(0, 20)}`,
+            title: `Artist: ${artistName.slice(0, 20)}`,
+            fixed: true,
+            font: { size: 23 },
+            color: { background: "#7f1734" },
           },
-          index
-        ) => {
-          artists = [
-            ...artists,
-            {
-              id: artistId,
-              label: artistName.slice(0, 20),
-              title: artistName.slice(0, 20),
-              fixed: true,
-              font: { size: 23 },
-            },
-          ];
-          albums = [
-            ...albums,
-            {
-              id: albumId,
-              label: albumName.slice(0, 20),
-              title: albumName.slice(0, 20),
-              from: albumId, /// just to be more readile
-              to: artistId,
-              font: { size: 18 },
-              widthConstraint: 150,
-            },
-          ];
-          songs = [
-            ...songs,
-            {
-              id: trackId,
-              label: trackName.slice(0, 20),
-              title: trackName.slice(0, 20),
-              from: trackId,
-              to: albumId,
-              shape: "circularImage",
-              image: trackImage,
-              font: { size: 15 },
-            },
-          ];
-        }
-      );
+        ];
+        artists.forEach((artist) => {
+          artists.forEach((artist2) => {
+            if (artist.label === artist2.label && artist2.id !== artist.id) {
+              artist2.id = artist.id;
+              artist2.label = artist.label;
+            }
+          });
+        });
+
+        albums = [
+          ...albums,
+          {
+            id: albumId,
+            label: `Album: ${albumName.slice(0, 20)}`,
+            title: `Album: ${albumName.slice(0, 20)}`,
+            from: albumId, /// just to be more readile
+            to: artists[index].id,
+            font: { size: 18 },
+            widthConstraint: 150,
+            hidden: false,
+          },
+        ];
+        songs = [
+          ...songs,
+          {
+            id: trackId,
+            label: trackName.slice(0, 20),
+            title: trackName.slice(0, 20),
+            from: trackId,
+            to: albumId,
+            artistID: artists[index].id,
+            shape: "circularImage",
+            image: trackImageIcon,
+            font: { size: 15 },
+          },
+        ];
+      }
+    );
 
     //// GET TOP 5 FAVORITE ARTISTS
-    // let mostFive = [];
+    let mostFive = [];
     let toArtists = [];
     albums.forEach((album) => (toArtists = [...toArtists, album.to]));
-    toArtists = [...new Set(toArtists)];
-    alert(toArtists);
+    // toArtists = [...new Set(toArtists)];
+    let lengthOne = false;
+    while (toArtists.length && !lengthOne) {
+      let node = toArtists[0];
+      let howFrequant = 1;
+
+      for (let i = 1; i < toArtists.length; i++)
+        if (toArtists[i] === node) howFrequant++;
+
+      mostFive = [...mostFive, [node, howFrequant]];
+      toArtists = toArtists.filter((toArtist) => node !== toArtist);
+
+      if (toArtists.length === 1) {
+        lengthOne = true;
+        mostFive = [...mostFive, [toArtists[0], 1]];
+      }
+    }
+    // alert(mostFive);
+    mostFive = mostFive
+      .sort((a, b) => a[1] < b[1])
+      .splice(0, 5)
+      .map((a) => a[0]);
 
     /// get unique values function, WORKS ONLY FOR ARRAY OF OBJECTS
     const unique = (ARR = [], KEY) => [
@@ -82,14 +113,9 @@ const Statistics = () => {
 
     // returns unique elements in the arrays
     artists = unique(artists, "id");
+    artists = artists.filter((artist) => mostFive.includes(artist.id));
     albums = unique(albums, "id");
-
-    /* 
-     SONGS IN ALBUM > 1 ? 
-      SONG.TO = ALBUM.ID : 
-      SONGS.TO = ALBUM.TO
-
-*/
+    /// SONGS IN ALBUM > 1 ? SONG.TO = ALBUM.ID : SONG.TO = ALBUM.TO (== ARTIST.ID)
     let TOs = [];
     songs.forEach((song) => (TOs = [...TOs, song.to]));
     TOs.forEach((to, idx) => {
@@ -98,6 +124,10 @@ const Statistics = () => {
         albums.forEach((album) => {
           if (to === album.from) {
             songs[idx].to = album.to;
+            delete album.from;
+            album.widthConstraint = 0;
+            album.hidden = true;
+
             delete album.to;
             delete album.label;
           }
@@ -106,6 +136,10 @@ const Statistics = () => {
       }
       TOs[idx] = to;
     });
+
+    songs = songs.filter(
+      (song) => mostFive.includes(song.to) || mostFive.includes(song.artistID)
+    );
 
     /// wrap all edges in an array
     [albums, songs].forEach((node) => {
@@ -130,7 +164,8 @@ const Statistics = () => {
       nodes: {
         borderWidth: 0,
         borderWidthSelected: 0,
-        margin: 1,
+        heightConstraint: true,
+        margin: 10,
         shape: "box",
         color: {
           border: "#000000e8",
@@ -152,7 +187,10 @@ const Statistics = () => {
       layout: {
         hierarchical: {
           enabled: true,
-          sortMethod: "directed",
+          // sortMethod: "directed",
+          direction: "DU",
+          // blockShifting: false,
+          // edgeMinimization: false,
         },
       },
     };
